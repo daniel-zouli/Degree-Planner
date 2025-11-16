2fcdbf6c8e8464a0b5470398fed9d56ad7073879
ckwok07 <christiankkwok910@gmail.com>
2025-11-15 20:39:41 -0800

    added 1 course CPSC110

--- data/courses.ts
+++ data/courses.ts
@@ -1,3 +1,16 @@
 import { Course } from '@/types';
 
-export const courses: Course[] = [];
+export const courses: Course[] = [
+    {
+        id: 'cpsc110',
+        code: 'CPSC 110',
+        name: 'Computation, Programs, and Programming',
+        credits: 4,
+        description: 'Fundamental program and computation structures. Introductory programming skills.',
+        prerequisites: [],
+        corequisites: [],
+        yearLevel: 1,
+        term: 'any',
+        category: 'required',
+      }
+];
