b6e52d5c4c759a7f4622a62ae9856ab30ff5dc3b
ckwok07 <christiankkwok910@gmail.com>
2025-11-15 21:38:48 -0800

    bug fix in new semester

--- components/ScheduleBuilder.tsx
+++ components/ScheduleBuilder.tsx
@@ -36,7 +36,7 @@ export default function ScheduleBuilder({
       courses: [],
     };
 
-    onSemestersChange([...semesters, newSemester]);
+    onSemestersChange([newSemester, ...semesters]);
     setShowAddSemester(false);
     onActiveSemesterChange(newSemester.id);
   };
